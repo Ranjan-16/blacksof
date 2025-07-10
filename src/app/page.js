42d5a8c4e9ba6ac242e5baf679bf4d9fb1f511e1
Ashishranjan Prasad <85986481+Ranjan-16@users.noreply.github.com>
2025-07-10 11:18:56 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
       <Header />
       <main>
         <HeroVideoSection />
-        <ContactSection />
         <AutomotiveSection />
+        <ContactSection />
       </main>
       <Footer />
     </>
